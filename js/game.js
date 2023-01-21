fc1f22e2b43920fae5119189dde3dd269c41cbbf
Hosseinreza <hrakbari77@gmail.com>
2023-01-21 20:54:07 +0330

    Add sound player

--- js/game.js
+++ js/game.js
@@ -6,6 +6,11 @@ function nextSequence() {
     randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     console.log(gamePattern);
+    $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+    
+   
 }
 
 nextSequence();

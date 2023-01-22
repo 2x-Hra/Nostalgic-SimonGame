8776a2568877ead3b77ec64f6eff6855a678c883
Hosseinreza <hrakbari77@gmail.com>
2023-01-22 22:08:51 +0330

    fix checkAnswer()

--- js/game.js
+++ js/game.js
@@ -15,17 +15,21 @@ $(document).on("keydown",function(){
 
     }
 })
-
+// This will Start the game and Choose random color 
 $(".btn").on("click", function(event){
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     console.log(userClickedPattern );
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    var lastAnswerIndex = buttonColors.indexOf(userChosenColor);
+    console.log("last index ::"+ lastAnswerIndex);
+    checkAnswer(lastAnswerIndex);
 });
 
 
 function nextSequence() {
+    userClickedPattern = [];
     level ++;
     $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4 );
@@ -51,5 +55,21 @@ function animatePress( currentColor){
 }
 
 function checkAnswer( currentLevel ){
+   
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
+        console.log("success");
+  
+        if (userClickedPattern.length === gamePattern.length){
+  
+        
+            setTimeout(function () {
+            nextSequence();
+            }, 1000);
+        }
+  
+    }
+    else{
+        console.log("wrong");
+    }
 }

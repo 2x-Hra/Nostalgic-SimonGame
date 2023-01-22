9b375adf8c35b80ac50dd30be4aee1f541e95539
Hosseinreza <hrakbari77@gmail.com>
2023-01-22 21:36:08 +0330

    Fix the start value

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-
+var startValue = 0 ;
+var level = 0;
 $(".btn").on("click", function(event){
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
@@ -33,3 +34,15 @@ function animatePress( currentColor){
         theButton.removeClass("pressed")
     }, 100);
 }
+$(document).ready(function(){
+    $(document).on("click",function(){
+        if(startValue === 0){
+            startValue = 1;
+            $("h1").text("Level "+ level);
+            nextSequence();
+        }
+        else{
+    
+        }
+    })
+})

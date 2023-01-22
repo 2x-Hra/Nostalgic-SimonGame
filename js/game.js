7efc33a567362134f96091e4ad566cd765b1e61c
Hosseinreza <hrakbari77@gmail.com>
2023-01-22 21:24:29 +0330

    Add animatePress func

--- js/game.js
+++ js/game.js
@@ -1,6 +1,16 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+
+$(".btn").on("click", function(event){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern );
+    playSound(userChosenColor);
+    
+});
+
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4 );
     console.log(randomNumber);
@@ -15,8 +25,7 @@ function playSound(name){
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
-$(".btn").on("click", function(event){
-    var userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern );
-});
+
+function animatePress( currentColor){
+    $("#"+currentColor).addClass("pressed");
+}
